a34767bf997fb106c5a7f22e26fb516bb2d1311b
Ubuntu <ubuntu@ip-172-31-21-224.us-east-2.compute.internal>
2021-11-15 21:16:30 +0000

    Adjust indent of program

--- assignment4/node_graph.py
+++ assignment4/node_graph.py
@@ -4,22 +4,22 @@ import string
 NODE_COUNT_PER_LAYER = [4,3,2]
 
 class Node:
+
     def __init__(self):
         self.children = []
         self.children_connection_weights = []
         self.node_name = ''.join([random.choice(string.ascii_letters) for i in range(3)])
+
     def make_children(self, current_layer_number, node_per_layer):
         #terminate the recursion
         if current_layer_number >= len(node_per_layer):
             return
-
         for i in range(node_per_layer[current_layer_number]):
             self.children.append(Node())
-        
         self.children[0].make_children(current_layer_number + 1, node_per_layer)
-
         for i in range(1, len(self.children)):
             self.children[i].children = self.children[0].children[:]
+
     def adjust_child_weights(self, current_layer_number, node_per_layer):
         if current_layer_number >= len(node_per_layer):
             return
@@ -35,7 +35,7 @@ class Node:
         if layer >= len(node_per_layer):
             print(f"{indent}{self.node_name} is connected to")
             return
-        print(f"{indent}{self.node_name}")
+        print(f"{indent}{self.node_name} is connected to")
         for i in range (len(self.children)):
             try:
                 print(f"{indent}with weight {self.children_connection_weights[i]}")
